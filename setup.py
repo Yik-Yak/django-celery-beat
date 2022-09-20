dffabb14eb2e672d8a9bc0e8ed91c4798b0ab14d
Marc Gibbons <1726961+marcgibbons@users.noreply.github.com>
2022-09-20 16:09:28 -0400

    Remove dependency on Django

--- setup.py
+++ setup.py
@@ -140,7 +140,7 @@ setuptools.setup(
     url=meta['homepage'],
     platforms=['any'],
     license='BSD',
-    install_requires=reqs('default.txt') + reqs('runtime.txt'),
+    install_requires=reqs('default.txt'),
     tests_require=reqs('test.txt') + reqs('test-django.txt'),
     cmdclass={'test': pytest},
     classifiers=classifiers,
